0012e89c70ef6c70cde49dca57b9aa84ded0913f
Juri Wiechmann <43830164+AncientCrimsonSnow@users.noreply.github.com>
2024-01-23 16:30:04 +0100

    Add TimeCondition

---
+++ Runtime/Transitions/Conditions/TimeCondition.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System.Timers;
+
+namespace StateMachinePackage.Runtime.Transitions.Conditions
+{
+    public class TimeCondition : Condition
+    {
+        private Timer _timer;
+
+        public TimeCondition(float seconds)
+        {
+            _timer = new Timer(seconds * 1000);
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.AutoReset = false;
+        }
+
+        public void StartTimer()
+        {
+            _timer.Start();
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            TriggerConditionMeetWrapper();
+        }
+
+        private void TriggerConditionMeetWrapper()
+        {
+            TriggerConditionMeet();
+        }
+    }
+}
